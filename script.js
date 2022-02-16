00d147eceaf220d7f35081b30f4e852f1a9b0f52
Roy Schrauwen <roy@royschrauwen.nl>
2022-02-16 19:08:01 +0100

    Cleaning up code

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* ================================ */
+/*           DECLARATIONS           */
+/* ================================ */
 // References
 const timerDiv = document.querySelector("#timer");
 const statusField = document.querySelector("#status");
@@ -13,7 +16,7 @@ const breakColor = "#7FFFD4";
 
 // Time in minutes. Can be inputted in later version
 const workTimeInMinutes = 0.05; // Default 25
-const shortBreakTimeInMinutes = 0.1; // Default 3
+const shortBreakTimeInMinutes = 0.05; // Default 3
 const longBreakTimeInMinutes = 3; // Default 30
 
 // Make seconds from the times.
@@ -25,17 +28,17 @@ let newStatus;
 
 const defaultSequence = 8;
 let currentSequence = defaultSequence;
-const sequenceNames = [
-  "End",
-  "Long Break!",
-  "Work 4",
-  "Short Break 3",
-  "Work 3",
-  "Short Break 2",
-  "Work 2",
-  "Short Break 1",
-  "Work 1",
-];
+// const sequenceNames = [
+//   "End",
+//   "Long Break!",
+//   "Work 4",
+//   "Short Break 3",
+//   "Work 3",
+//   "Short Break 2",
+//   "Work 2",
+//   "Short Break 1",
+//   "Work 1",
+// ];
 
 var currentTime = workTimeInSeconds;
 
@@ -54,7 +57,9 @@ const statusMessageBreak = [
   "Time for a short walk",
 ];
 
-/* ========== FUNCTIONS ========== */
+/* ================================ */
+/*             FUNCIONS             */
+/* ================================ */
 
 // Ik vind het er mooier uit zien als getallen een voorloopnul hebben
 function addLeadingZero(number) {
@@ -71,16 +76,9 @@ function updateTimer() {
       addLeadingZero(currentTime % 60);
     currentTime--;
   } else {
-    console.log("===========================================");
     clearInterval(timeInterval);
     currentSequence--;
-    console.log(
-      "currentSequence: " +
-        sequenceNames[currentSequence] +
-        " (" +
-        currentSequence +
-        ")"
-    );
+
     if (currentSequence > 0) {
       updateStatusField();
       if (currentSequence % 2 == 0) {
@@ -91,7 +89,6 @@ function updateTimer() {
     } else {
       console.log("EINDE TIMER! START MAAR OPNIEUW!");
       clearInterval(timeInterval);
-      //startTimer();
     }
   }
 }
@@ -101,14 +98,7 @@ function startTimer() {
   startButton.textContent = "Pause Timer";
   currentSequence = defaultSequence;
   updateStatusField();
-  console.log("START TIMER!");
-  console.log(
-    "currentSequence: " +
-      sequenceNames[currentSequence] +
-      " (" +
-      currentSequence +
-      ")"
-  );
+
   currentlyWorking = 1;
   currentWorkTime = workTimeInSeconds;
   timeInterval = window.setInterval(updateTimer, 1000);
@@ -117,7 +107,6 @@ function startTimer() {
 function startShortBreak() {
   currentTime = shortBreakTimeInSeconds;
   var temp = defaultSequence - currentSequence;
-  //   console.log("startShortBreak(): defaultSequence - currentSequence: " + temp);
   if (defaultSequence - currentSequence == defaultSequence - 1) {
     currentTime = longBreakTimeInSeconds;
   }
@@ -131,15 +120,6 @@ function startWorking() {
 }
 
 function updateStatusField() {
-  //   var i = defaultSequence - currentSequence;
-  //   console.log("updateStatusField(): defaultSequence - currentSequence: " + i);
-  //   console.log(
-  //     "updateStatusField(): currentSequence: " +
-  //       sequenceNames[currentSequence] +
-  //       " (" +
-  //       currentSequence +
-  //       ")"
-  //   );
   markers[defaultSequence - currentSequence].style.backgroundColor = "#FF00FF";
   if (defaultSequence - currentSequence >= 1) {
     markers[defaultSequence - currentSequence - 1].style.backgroundColor =
